83438975d418fcf5238170ceb1877ac8e58578ba
Dennis Emannuel de Paiva Lopes <dennisemannuel93@gmail.com>
2025-01-19 14:27:52 -0300

    atualizações

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,19 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const webpack = require('webpack');
+const TerserPlugin = require('terser-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const { PurgeCSSPlugin } = require('purgecss-webpack-plugin');
+const glob = require('glob');
 require('dotenv').config();
 
-module.exports = {
+const PATHS = {
+  src: path.join(__dirname, 'src')
+};
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+const config = {
   entry: {
     main: './src/js/main.js',
     relatorio: './src/js/relatorio.js',
@@ -12,7 +22,8 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name].bundle.js',
+    filename: '[name].[contenthash].js',
+    clean: true,
   },
   module: {
     rules: [
@@ -21,12 +32,16 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-syntax-dynamic-import']
+          }
         },
       },
       {
         test: /\.css$/,
         use: [
-          MiniCssExtractPlugin.loader,
+          isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
           'css-loader',
           'postcss-loader'
         ],
@@ -49,11 +64,15 @@ module.exports = {
       filename: 'resultados.html',
       chunks: ['resultados']
     }),
-    new MiniCssExtractPlugin({
-      filename: 'styles.[contenthash].css',
-    }),
     new webpack.DefinePlugin({
-      'process.env': JSON.stringify(process.env)
+      'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
+      'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+      'process.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.FIREBASE_DATABASE_URL),
+      'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+      'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+      'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID),
+      'process.env.FIREBASE_APP_ID': JSON.stringify(process.env.FIREBASE_APP_ID),
+      'process.env.FIREBASE_MEASUREMENT_ID': JSON.stringify(process.env.FIREBASE_MEASUREMENT_ID)
     }),
   ],
   devServer: {
@@ -63,14 +82,44 @@ module.exports = {
     compress: true,
     port: 9000,
   },
-  stats: {
-    errors: true,
-    warnings: true,
-    errorDetails: true,
-  },
   optimization: {
     splitChunks: {
       chunks: 'all',
+      maxInitialRequests: Infinity,
+      minSize: 0,
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name(module) {
+            const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+            return `npm.${packageName.replace('@', '')}`;
+          },
+        },
+      },
     },
   },
 };
+
+if (isProduction) {
+  config.plugins.push(
+    new MiniCssExtractPlugin({
+      filename: 'styles.[contenthash].css',
+    }),
+    new PurgeCSSPlugin({
+      paths: glob.sync(`${PATHS.src}/**/*`, { nodir: true }),
+    })
+  );
+
+  config.optimization.minimizer = [
+    new TerserPlugin(),
+    new CssMinimizerPlugin(),
+  ];
+
+  config.performance = {
+    hints: 'warning',
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000,
+  };
+}
+
+module.exports = config;
